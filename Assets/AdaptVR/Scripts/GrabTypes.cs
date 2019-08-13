fb08a3f014f2686ee040db9e07218a3d2ad0441e
Arthur Souza <arsbr.souza@gmail.com>
2019-08-13 15:46:13 -0300

    Intial commit

---
+++ Assets/AdaptVR/Scripts/GrabTypes.cs
@@ -0,0 +1,18 @@
+﻿//======= Copyright (c) Valve Corporation, All rights reserved. ===============
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AdaptVR
+{
+    public enum GrabTypes
+    {
+        None,
+        Trigger,
+        Pinch,
+        Grip,
+        Scripted,
+    }
+}
